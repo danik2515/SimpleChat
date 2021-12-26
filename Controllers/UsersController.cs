3dae0e2f1a6164fe020f31882e873f19217b6686
danik2515 <zahdanik@gmail.com>
2021-12-27 01:19:29 +0300

    fixed multiple delete

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -24,30 +24,34 @@ namespace WebApp.Controllers {
                 if (!_userManager.FindByNameAsync(currentUser).Result.IsBlocked) {
                     
                         foreach (UserModel User in model.UserList) {
-                            if (User.IsChecked) {
-                                var user = _userManager.FindByNameAsync(User.User.UserName).Result;
-                                if (button.Equals("block")) {
+                        
+                        if (User.IsChecked) {
+                            var user = _userManager.FindByNameAsync(User.User.UserName).Result;
+                            if (button.Equals("delete")) {
+                                if (user.UserName.Equals(currentUser)) { await _signInManager.SignOutAsync(); }
+                                await _userManager.DeleteAsync(user);
+                                continue;
+                            }
+                            if (button.Equals("block")) {
                                     user.IsBlocked = true;
                                     if (user.UserName.Equals(currentUser)) {
                                         await _signInManager.SignOutAsync(); }
                                 }
 
                                 if (button.Equals("unblock")) {user.IsBlocked = false;}
-                                if (button.Equals("delete")) {
-                                    if (user.UserName.Equals(currentUser)) {await _signInManager.SignOutAsync();}
-                                    await _userManager.DeleteAsync(user);
-                                }
+                                
                                 if (button.Equals("sendMessage")) { user.Messeges += "Message from " + currentUser + ":  " + message + "\n"; }
                                 if (button.Equals("giveRight")) { await _userManager.AddToRoleAsync(user, privilege); }
                                 if (button.Equals("deleteRight")) { await _userManager.RemoveFromRoleAsync(user, privilege); }
-                                    await _userManager.UpdateAsync(user);
-                                
-                            }
+                            await _userManager.UpdateAsync(user);
 
                         }
+                        
+                    }
                     
-                    
-                }
+
+
+                    }
                 else {
                     await _signInManager.SignOutAsync();
                 }
